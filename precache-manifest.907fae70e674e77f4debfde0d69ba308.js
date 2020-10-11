3cf68a69d248dea7735c65d8f0b3dc1a192e85f4
themikegit <rostovniki5@gmail.com>
2020-10-11 22:24:46 +0200

    Updates

---
+++ precache-manifest.907fae70e674e77f4debfde0d69ba308.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2a498ad03359b4c53e6a04feaced56",
+    "url": "/activitie_track/index.html"
+  },
+  {
+    "revision": "1b7a8e2069332eebf2b8",
+    "url": "/activitie_track/static/css/main.05cadac6.chunk.css"
+  },
+  {
+    "revision": "81939bb3792181e803f8",
+    "url": "/activitie_track/static/js/2.5249d9ba.chunk.js"
+  },
+  {
+    "revision": "d60080142374a7f039f21d1c88fd0d50",
+    "url": "/activitie_track/static/js/2.5249d9ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b7a8e2069332eebf2b8",
+    "url": "/activitie_track/static/js/main.2aaeaff6.chunk.js"
+  },
+  {
+    "revision": "75cd9a6ba4f328345e35",
+    "url": "/activitie_track/static/js/runtime-main.2ad5270c.js"
+  }
+]);
